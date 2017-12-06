8662b773cf2cc3dd7967043cae405da89d8a43f8
Mohammed El-Kebir <melkebir@cs.princeton.edu>
2017-12-06 14:07:18 -0500

    Downsampling dataset

---
+++ data/sims/m5_downsampled/R/downsample.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ ! $# -eq 1 ]
+then
+    echo "Usage: $0 <downsample_executable>" >&2
+    exit 1
+fi
+
+for s in {1036,1275,1381,1394,1593,1708,219,414,759,769}
+do
+    for C in {60,200,10000}
+    do 
+        for P in {0.5,0.8,1.0}
+        do
+            for k in {1,2,4,5}
+            do
+                $1 reads_seed${s}.tsv -C $C -k $k -P $P -E 0.001 > reads_seed${s}_k${k}_P${P}_C${C}.tsv
+            done
+        done
+    done
+done
